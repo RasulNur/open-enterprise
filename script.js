83eac8b8854fc392c2898375e6f9ef2453185c65
RasulNur <rasulnuriyev2004@gmail.com>
2023-08-26 15:35:00 +0500

    add no draggable, fix style.css

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 const headerMenu = document.querySelector(".header__menu");
 const navList = document.querySelector(".header__nav-list");
 
+function setDraggableOff(tag) {
+    const arr = document.querySelectorAll(`${tag}`);
+
+    arr.forEach((el) => {
+        el.setAttribute("draggable", false);
+    });
+}
+
+setDraggableOff("img");
+setDraggableOff("a");
+
 headerMenu.addEventListener("click", () => {
     headerMenu.classList.toggle("active");
     navList.classList.toggle("active");
-    if (navList.classList.contains("active"))
-        document.body.style.overflowY = "hidden";
-    else document.body.style.overflowY = "scroll";
+
+    if (navList.classList.contains("active")) {
+        document.body.style.userSelect = "none";
+        document.body.classList.add("noscroll");
+    } else {
+        document.body.classList.remove("noscroll");
+        document.body.style.userSelect = "auto";
+    }
 });
 
 const headerLinks = document.querySelectorAll(".header__list-link");
@@ -15,5 +31,6 @@ headerLinks.forEach((link) => {
     link.addEventListener("click", () => {
         headerMenu.classList.remove("active");
         navList.classList.remove("active");
+        document.body.classList.remove("noscroll");
     });
 });
